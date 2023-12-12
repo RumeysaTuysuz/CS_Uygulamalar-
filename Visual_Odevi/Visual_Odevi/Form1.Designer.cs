aa671aec3b1ced61e7c0beb7e1f43a333ec7dbc3
RumeysaTuysuz <rumeysatysz2002@gmail.com>
2023-12-13 01:53:37 +0300

    Homework

---
+++ Visual_Odevi/Visual_Odevi/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Visual_Odevi
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.txtId = new System.Windows.Forms.TextBox();
+			this.txtName = new System.Windows.Forms.TextBox();
+			this.txtSurname = new System.Windows.Forms.TextBox();
+			this.txtMail = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.btnShow = new System.Windows.Forms.Button();
+			this.btnUpdate = new System.Windows.Forms.Button();
+			this.btnSave = new System.Windows.Forms.Button();
+			this.btnDelete = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(62, 39);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(23, 16);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "ID:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(43, 75);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(49, 16);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "NAME:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 118);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(78, 16);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "SURNAME:";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(52, 159);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(40, 16);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "MAIL:";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(41, 196);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(51, 16);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "LEVEL:";
+			// 
+			// txtId
+			// 
+			this.txtId.Location = new System.Drawing.Point(91, 36);
+			this.txtId.Name = "txtId";
+			this.txtId.Size = new System.Drawing.Size(175, 22);
+			this.txtId.TabIndex = 5;
+			// 
+			// txtName
+			// 
+			this.txtName.Location = new System.Drawing.Point(91, 72);
+			this.txtName.Name = "txtName";
+			this.txtName.Size = new System.Drawing.Size(175, 22);
+			this.txtName.TabIndex = 6;
+			// 
+			// txtSurname
+			// 
+			this.txtSurname.Location = new System.Drawing.Point(91, 115);
+			this.txtSurname.Name = "txtSurname";
+			this.txtSurname.Size = new System.Drawing.Size(175, 22);
+			this.txtSurname.TabIndex = 7;
+			// 
+			// txtMail
+			// 
+			this.txtMail.Location = new System.Drawing.Point(91, 156);
+			this.txtMail.Name = "txtMail";
+			this.txtMail.Size = new System.Drawing.Size(175, 22);
+			this.txtMail.TabIndex = 8;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "Manager",
+            "Admin",
+            "Ex-Member",
+            "Co-Manager",
+            "Beginner"});
+			this.comboBox1.Location = new System.Drawing.Point(91, 193);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(175, 24);
+			this.comboBox1.TabIndex = 9;
+			// 
+			// btnShow
+			// 
+			this.btnShow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+			this.btnShow.Location = new System.Drawing.Point(318, 101);
+			this.btnShow.Name = "btnShow";
+			this.btnShow.Size = new System.Drawing.Size(90, 49);
+			this.btnShow.TabIndex = 10;
+			this.btnShow.Text = "SHOW";
+			this.btnShow.UseVisualStyleBackColor = false;
+			this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+			// 
+			// btnUpdate
+			// 
+			this.btnUpdate.BackColor = System.Drawing.Color.Peru;
+			this.btnUpdate.Location = new System.Drawing.Point(510, 101);
+			this.btnUpdate.Name = "btnUpdate";
+			this.btnUpdate.Size = new System.Drawing.Size(90, 49);
+			this.btnUpdate.TabIndex = 11;
+			this.btnUpdate.Text = "UPDATE";
+			this.btnUpdate.UseVisualStyleBackColor = false;
+			this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+			// 
+			// btnSave
+			// 
+			this.btnSave.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.btnSave.Location = new System.Drawing.Point(414, 101);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(90, 49);
+			this.btnSave.TabIndex = 12;
+			this.btnSave.Text = "SAVE";
+			this.btnSave.UseVisualStyleBackColor = false;
+			this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+			// 
+			// btnDelete
+			// 
+			this.btnDelete.BackColor = System.Drawing.Color.PeachPuff;
+			this.btnDelete.Location = new System.Drawing.Point(606, 102);
+			this.btnDelete.Name = "btnDelete";
+			this.btnDelete.Size = new System.Drawing.Size(90, 49);
+			this.btnDelete.TabIndex = 13;
+			this.btnDelete.Text = "DELETE";
+			this.btnDelete.UseVisualStyleBackColor = false;
+			this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(129, 255);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersWidth = 51;
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(541, 158);
+			this.dataGridView1.TabIndex = 14;
+			this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.RosyBrown;
+			this.ClientSize = new System.Drawing.Size(817, 489);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.btnDelete);
+			this.Controls.Add(this.btnSave);
+			this.Controls.Add(this.btnUpdate);
+			this.Controls.Add(this.btnShow);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.txtMail);
+			this.Controls.Add(this.txtSurname);
+			this.Controls.Add(this.txtName);
+			this.Controls.Add(this.txtId);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox txtId;
+		private System.Windows.Forms.TextBox txtName;
+		private System.Windows.Forms.TextBox txtSurname;
+		private System.Windows.Forms.TextBox txtMail;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button btnShow;
+		private System.Windows.Forms.Button btnUpdate;
+		private System.Windows.Forms.Button btnSave;
+		private System.Windows.Forms.Button btnDelete;
+		private System.Windows.Forms.DataGridView dataGridView1;
+	}
+}
+
---
+++ Visual_Odevi/Visual_Odevi/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Visual_Odevi
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		OleDbConnection Connection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\"C:\\Users\\Rumeysa Tüysüz\\OneDrive\\Belgeler\\Database1.accdb\"");
+
+		private void showInformation()
+		{
+			DataTable dataTable = new DataTable();
+			OleDbDataAdapter dataAdapter = new OleDbDataAdapter("SELECT * FROM information", Connection);
+			dataAdapter.Fill(dataTable);
+			dataGridView1.DataSource = dataTable;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			showInformation();
+		}
+
+		private void btnShow_Click(object sender, EventArgs e)
+		{
+			showInformation();
+		}
+
+		private void btnSave_Click(object sender, EventArgs e)
+		{
+			Connection.Open();
+			OleDbCommand save = new OleDbCommand("INSERT INTO information (NAME,SURNAME,MAIL,mPosition) VALUES (@p1,@p2,@p3,@p4)", Connection);
+			save.Parameters.AddWithValue("@p1", txtName.Text);
+			save.Parameters.AddWithValue("@p2", txtSurname.Text);
+			save.Parameters.AddWithValue("@p3", txtMail.Text);
+			save.Parameters.AddWithValue("@p4", comboBox1.Text);
+
+			save.ExecuteNonQuery();
+			Connection.Close();
+			MessageBox.Show("Saved in the system.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+			txtId.Clear();
+			txtName.Clear();
+			txtSurname.Clear();
+			txtMail.Clear();
+			comboBox1.ResetText();
+
+			showInformation();
+		}
+
+		private void btnUpdate_Click(object sender, EventArgs e)
+		{
+			Connection.Open();
+			OleDbCommand update = new OleDbCommand("UPDATE information SET NAME=@p1,SURNAME=@p2,MAIL=@p3,mPosition=@p4 WHERE ID=@p5", Connection);
+			update.Parameters.AddWithValue("@p1", txtName.Text);
+			update.Parameters.AddWithValue("@p2", txtSurname.Text);
+			update.Parameters.AddWithValue("@p3", txtMail.Text);
+			update.Parameters.AddWithValue("@p4", comboBox1.Text);
+			update.Parameters.AddWithValue("@p5", txtId.Text);
+
+			update.ExecuteNonQuery();
+			Connection.Close();
+			MessageBox.Show("Updated in the system.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+			txtId.Clear();
+			txtName.Clear();
+			txtSurname.Clear();
+			txtMail.Clear();
+			comboBox1.ResetText();
+
+			showInformation();
+
+		}
+
+		private void btnDelete_Click(object sender, EventArgs e)
+		{
+			Connection.Open();
+			OleDbCommand delete = new OleDbCommand("DELETE FROM information WHERE ID=@p1", Connection);
+			delete.Parameters.AddWithValue("@p1", txtId.Text);
+			delete.ExecuteNonQuery();
+			Connection.Close();
+			MessageBox.Show("Deleted in the system.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+			txtId.Clear();
+			txtName.Clear();
+			txtSurname.Clear();
+			txtMail.Clear();
+			comboBox1.ResetText();
+
+			showInformation();
+
+		}
+
+		private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+			int select = dataGridView1.SelectedCells[0].RowIndex;
+
+			txtId.Text = dataGridView1.Rows[select].Cells[0].Value.ToString();
+			txtName.Text = dataGridView1.Rows[select].Cells[1].Value.ToString();
+			txtSurname.Text = dataGridView1.Rows[select].Cells[2].Value.ToString();
+			txtMail.Text = dataGridView1.Rows[select].Cells[3].Value.ToString();
+			comboBox1.Text = dataGridView1.Rows[select].Cells[4].Value.ToString();
+		}
+
+	}
+}
